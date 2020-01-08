91656a1a2217a0ad7d73968650b3dae45498e21f
TomasSeven <30586945+TomasSeven@users.noreply.github.com>
2020-01-08 16:27:49 +0100

    new version
    updated

--- src/index.js
+++ src/index.js
@@ -1,47 +1,110 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import cleanreg from "./images/cleanreg.jpg";
+import "./styles.css";
+
+class DisplayImg extends React.Component {
+  render() {
+    const style = {
+      width: "500px",
+      height: "200px",
+      backgroundImage: "url(" + this.props.imgUrl + ")",
+      backgoundRepeat: "no-repeat",
+      backgroundSize: "500px 200px"
+    };
+
+    return <div style={style} />;
+  }
+}
+
+class DisplayNew extends React.Component {
+  render() {
+    const style = {
+      width: "800px",
+      height: "200px",
+      backgroundImage: "url(" + this.props.imgUrl + ")",
+      textAlign: "center"
+    };
+
+    return (
+      <div className="container">
+        <img src={cleanreg} alt="Snow" stylea="width:100%;" />
+        <div className="centered">{this.props.random}</div>
+      </div>
+    );
+  }
+}
 
 class Button extends React.Component {
-  
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = { random: 'ABC123' };
+    this.state = { random: "ABC123", random2: "CBA321" };
   }
 
   handleClick() {
-    this.state = { random: 'ABC123' };
     const min = 100;
     const max = 999;
-    const rand =  Math.random() * (max - min);
+    const rand = Math.floor(Math.random() * (max - min + 1)) + min;
     const rand2 = Math.floor(Math.random() * (max - min + 1)) + min;
-    const abc = Math.random().toString(36).substring(2, 15);
     var text = "";
-    var possible = "ABCDEFGHJKLMNOPQRSTUWXYZ";
-     
+    var text2 = "";
+    var possible = "ABCDEFGHJKLMNOPRSTUWXYZ";
+
     for (var i = 0; i < 3; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
-     
-    
-    this.setState({ random: text+ rand2});
+    for (var i = 0; i < 3; i++)
+      text2 += possible.charAt(Math.floor(Math.random() * possible.length));
+    this.setState({ random: text + rand, random2: text2 + rand2 });
+  }
+
+  DisplayImg() {
+    const style = {
+      backgroundImage: "url(" + this.props.imgUrl + ")",
+      textAlign: "left"
+    };
+
+    return <div style={style}> </div>;
+  }
+
+  DisplayNew() {
+    const style = {
+      backgroundImage: "url(" + this.props.imgUrl + ")",
+      position: relative
+    };
+
+    return <div style={style}> </div>;
   }
 
   render() {
     return (
       <div className="row">
         <div className="col-12">
-          <h3 className="text-center">React Random Number Generator</h3>
-          <p>Simple example for this question in Stackoverflow: <a href="https://stackoverflow.com/questions/45175836/random-number-using-react-js" target="_blank">https://stackoverflow.com/questions/45175836/random-number-using-react-js</a></p>
-          <button className="btn btn-primary" onClick={this.handleClick.bind(this)}>Click</button>
-          <h1 className="card" style={{marginTop:"10px"}}>
-            <div className="card-block">
-              {this.state.random}
-            </div>
-          </h1>
+          <h3 className="text-center">Generate random Regnumber</h3>
+          <button
+            className="btn btn-primary"
+            onClick={this.handleClick.bind(this)}
+          >
+            {" "}
+            Generate RegNo
+          </button>
+          <h1 className="card" style={{ marginTop: "10px" }} />
+
+          <div className="container">
+            <DisplayNew imgUrl={cleanreg} />
+            <div className="centered">{this.state.random}</div>
+          </div>
+        </div>
+        <h1 className="card" style={{ marginTop: "50px" }} />
+        <div>
+          <div className="container">
+            <img src={cleanreg} alt="Snow" stylea="width:100%;" />
+            <div className="centered">{this.state.random2}</div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-render(<Button />, document.getElementById('container'));
+render(<Button />, document.getElementById("container"));
